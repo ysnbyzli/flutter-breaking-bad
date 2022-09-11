36f68e95f4494e3ecf4c303b67058fb0cdb68593
Yasin Beyazlı <yasinbeyazli29@gmail.com>
2022-09-11 18:48:51 +0300

    feat: created theme colors

---
+++ lib/theme/color_theme.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ThemeColor {
+  static const Color darkGreen = Color(0x093009);
+  static const Color darkGreen800 = Color(0x0D3E10);
+  static const Color darkGreen600 = Color(0x1F6032);
+  static const Color darkGreen400 = Color(0x29773E);
+  static const Color darkGreen200 = Color(0x369457);
+  static const Color black = Color(0x032202);
+}
