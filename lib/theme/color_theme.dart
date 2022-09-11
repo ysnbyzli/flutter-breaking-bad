19219276ba43dfa68320ac87efa68db0cfc7651d
Yasin Beyazlı <yasinbeyazli29@gmail.com>
2022-09-11 19:07:06 +0300

    fix: color opactiy and added new color set 🎨

--- lib/theme/color_theme.dart
+++ lib/theme/color_theme.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 
 class ThemeColor {
-  static const Color darkGreen = Color(0x093009);
-  static const Color darkGreen800 = Color(0x0D3E10);
-  static const Color darkGreen600 = Color(0x1F6032);
-  static const Color darkGreen400 = Color(0x29773E);
-  static const Color darkGreen200 = Color(0x369457);
-  static const Color black = Color(0x032202);
+  static const Color darkGreen = Color(0xff093009);
+  static const Color darkGreen800 = Color(0xff0D3E10);
+  static const Color darkGreen600 = Color(0xff1F6032);
+  static const Color darkGreen400 = Color(0xff29773E);
+  static const Color darkGreen200 = Color(0xff369457);
+  static const Color black = Color(0xff032202);
+  static const Color red = Color(0xffe74c3c);
+  static const Color rise = Color(0xfffbc531);
 }
